bcfe42bff38c558155c3bd25aba257a60b0f9df4
Erwin Jocosing <erwinjocosing@gmail.com>
2023-05-11 20:16:49 -0700

    updated files

---
+++ README.txt
@@ -0,0 +1,33 @@
+# WeatherApp
+
+## User Story
+```
+AS A coding boot camp student
+I WANT to create a weather app that will display 5 days forecast
+```
+## Acceptance Criteria
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+## Usage
+
+To use the Work Day Schedule, type an event in a time slot and click the save button. You can also see the current time in red. Future time will be in green and past time is in grey.  If you refresh the page you will retain your input from the storage.
+
+https://ej619.github.io/ej619/WorkDayScheduler/
+
+## Credits
+
+Kimberly Carpenter Student. @ github.com/kimberlyc1904 
+Juan Delgado Tutor
+W3 Schools https://www.w3schools.com/
+ChatGPT https://chat.openai.com/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var userFormEl = document.querySelector('#user-form');
-// var languageButtonsEl = document.querySelector('#language-buttons');
 var nameInputEl = document.querySelector('#city');
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
@@ -19,21 +18,10 @@ var formSubmitHandler = function (event) {
   }
 };
 
-/*var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
-  }
-};*/
-
 var getForecast = function (city) {
   var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=334c9cf8dddd1102356e9a0fff7d603c';
   console.log(apiUrl);
   
-
   fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
@@ -50,20 +38,6 @@ var getForecast = function (city) {
     });
 };
 
-/*var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};*/
-
 var displayForecast = function (forecast, city) {
   console.log('Displaying forecast')
   if (forecast.length === 0) {
@@ -74,40 +48,28 @@ var displayForecast = function (forecast, city) {
   weatherSearchTerm.textContent = city;
 
   for (var i = 0; i < forecast.list.length; i+=8) {
-    //var repoName = repos[i].owner.login + '/' + repos[i].name;
-    //console.log(forecast)
     var day = forecast.list[i].dt_txt;
     var temp = forecast.list[i].main.temp;
     var humidity = forecast.list[i].main.humidity;
     var wind =  forecast.list[i].wind.speed;
-
-
+    var iconCode = forecast.list[i].weather[0].icon;
+    var iconUrl = 'http://openweathermap.org/img/w/' + iconCode + '.png'; 
     console.log('day: '+ day, ' temp: '+ temp + ' humidity: ' + humidity + ' wind: ' + wind);
-
     var forecastEl = document.createElement('div');
     forecastEl.classList = 'list-item flex-row justify-space-between align-center';
-
+    var iconEl = document.createElement('img');
+    iconEl.setAttribute('src', iconUrl);
     var titleEl = document.createElement('span');
-    titleEl.textContent = city;
-    //console.log(titleEl);
-
-    forecast.appendChild(titleEl);
-
+    forecastEl.appendChild(titleEl);
     var statusEl = document.createElement('span');
     statusEl.classList = 'flex-row align-center';
-
-    /*if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }*/
-
-    forecast.appendChild(statusEl);
-
+    titleEl.textContent = day + ': ' + temp + ' F, ' + humidity + '% humidity, ' + wind + ' mph wind';
+    statusEl.appendChild(iconEl);
+    forecastEl.appendChild(statusEl);
     weatherContainerEl.appendChild(forecastEl);
+    localStorage.setItem(city, city);
   }
 };
 
 userFormEl.addEventListener('submit', formSubmitHandler);
-//languageButtonsEl.addEventListener('click', buttonClickHandler);
+
